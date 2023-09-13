ec932efd26492d1a3294973de3be8d0842ab6f30
Md Rijwan Jannat <you@example.com>
2023-09-14 02:01:37 +0600

    deploy

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
+
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
-import morgan from "morgan";
-import { Configuration, OpenAIApi } from "openai";
+import morgan from 'morgan';
 import openAiRoutes from "./routes/openai.js";
 import authRoutes from "./routes/auth.js";
+import { OpenAI } from 'openai';
+
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -19,18 +21,27 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-/* OPEN AI CONFIGURATION */
-const configuration = new Configuration({
-  apiKey: process.env.OPEN_API_KEY,
-});
-export const openai = new OpenAIApi(configuration);
+
+// export const openai = new OpenAI({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
+
+
+// Your OpenAI API key
+const apiKey = 'sk-zkk255md977svwGV3CSpT3BlbkFJ7A0aW112DXA1z8mLYOM';
+
+export const openai = new OpenAI({ apiKey });
+
+// Now you can use the `openai` object to make API requests.
+
+
 
 /* ROUTES */
 app.use("/openai", openAiRoutes);
 app.use("/auth", authRoutes);
 
 /* SERVER SETUP */
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 9000
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
 });
